fb551aab56e1b718f6cacdeb98fd1052e8f7752d
ashwini amrutkar <ashwiniamrutkar2000@gmail.com>
2021-04-08 02:11:58 +0530

    Recycler View done.

--- app/src/main/java/com/example/concretecrackrepairguide/CrackAdapter/CrAdapter.java
+++ app/src/main/java/com/example/concretecrackrepairguide/CrackAdapter/CrAdapter.java
@@ -1,5 +1,7 @@
 package com.example.concretecrackrepairguide.CrackAdapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,24 +10,41 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.concretecrackrepairguide.Model.CrackModel;
 import com.example.concretecrackrepairguide.R;
 
+import java.util.ArrayList;
+
 public class CrAdapter extends RecyclerView.Adapter<CrAdapter.ViewHolder1>{
+
+    ArrayList<CrackModel> list;
+    Context cnt;
+
+    public CrAdapter(ArrayList<CrackModel> list, Context cnt) {
+        this.list = list;
+        this.cnt = cnt;
+    }
 
     @NonNull
     @Override
     public ViewHolder1 onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(cnt).inflate(R.layout.sample,parent,false);
+        return new ViewHolder1(view);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder1 holder, int position) {
-
+        CrackModel model = list.get(position);
+        holder.img.setImageResource(model.getPic());
+        holder.txt.setText(model.getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
+
+
     }
 
     public class ViewHolder1 extends RecyclerView.ViewHolder {
