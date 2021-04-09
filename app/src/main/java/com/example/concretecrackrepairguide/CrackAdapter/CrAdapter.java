d626860aee16e57456c868ae269684119be1c034
ashwini amrutkar <ashwiniamrutkar2000@gmail.com>
2021-04-09 15:20:26 +0530

    minor changes.

--- app/src/main/java/com/example/concretecrackrepairguide/CrackAdapter/CrAdapter.java
+++ app/src/main/java/com/example/concretecrackrepairguide/CrackAdapter/CrAdapter.java
@@ -15,7 +15,33 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.concretecrackrepairguide.Activities.CorrosionCrackBeam;
 import com.example.concretecrackrepairguide.Activities.CorrosionCrackBeam1;
 import com.example.concretecrackrepairguide.Activities.CorrosionCrackColumn;
+import com.example.concretecrackrepairguide.Activities.CrazedConcrete1;
+import com.example.concretecrackrepairguide.Activities.Crazing1;
 import com.example.concretecrackrepairguide.Activities.DiagonalCrack;
+import com.example.concretecrackrepairguide.Activities.DiagonalCrack1;
+import com.example.concretecrackrepairguide.Activities.DiagonalCrack2;
+import com.example.concretecrackrepairguide.Activities.Expansion;
+import com.example.concretecrackrepairguide.Activities.Expansion1;
+import com.example.concretecrackrepairguide.Activities.FlexureCrack;
+import com.example.concretecrackrepairguide.Activities.FlexureCrack1;
+import com.example.concretecrackrepairguide.Activities.FlexureCrack2;
+import com.example.concretecrackrepairguide.Activities.FlexureCrack3;
+import com.example.concretecrackrepairguide.Activities.FlexureCrack4;
+import com.example.concretecrackrepairguide.Activities.HeavingConcreteCrack;
+import com.example.concretecrackrepairguide.Activities.HeavingConcreteCrack1;
+import com.example.concretecrackrepairguide.Activities.HorizontalCrack;
+import com.example.concretecrackrepairguide.Activities.HorizontalCrack1;
+import com.example.concretecrackrepairguide.Activities.Overload;
+import com.example.concretecrackrepairguide.Activities.PlasticShrinkage;
+import com.example.concretecrackrepairguide.Activities.PlasticShrinkage1;
+import com.example.concretecrackrepairguide.Activities.PrematureDrying;
+import com.example.concretecrackrepairguide.Activities.Settling;
+import com.example.concretecrackrepairguide.Activities.Settling1;
+import com.example.concretecrackrepairguide.Activities.ShearCrack;
+import com.example.concretecrackrepairguide.Activities.SteppedCrack;
+import com.example.concretecrackrepairguide.Activities.SteppedCrack1;
+import com.example.concretecrackrepairguide.Activities.SteppedCrack2;
+import com.example.concretecrackrepairguide.Activities.TorsionalCrack;
 import com.example.concretecrackrepairguide.MainActivity;
 import com.example.concretecrackrepairguide.Model.CrackModel;
 import com.example.concretecrackrepairguide.R;
@@ -64,15 +90,114 @@ public class CrAdapter extends RecyclerView.Adapter<CrAdapter.ViewHolder1>{
                         i = new Intent(cnt, CorrosionCrackColumn.class);
                         cnt.startActivity(i);
                         break;
+                    case 3:
+                        i = new Intent(cnt, CrazedConcrete1.class);
+                        cnt.startActivity(i);
+                        break;
                     case 4:
-                        i = new Intent(cnt, CorrosionCrackBeam.class);
+                        i = new Intent(cnt, Crazing1.class);
                         cnt.startActivity(i);
                         break;
                     case 5:
                         i = new Intent(cnt, DiagonalCrack.class);
                         cnt.startActivity(i);
                         break;
-
+                    case 6:
+                        i = new Intent(cnt, DiagonalCrack1.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 7:
+                        i = new Intent(cnt, DiagonalCrack2.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 8:
+                        i = new Intent(cnt, Expansion.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 9:
+                        i = new Intent(cnt, Expansion1.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 10:
+                        i = new Intent(cnt, FlexureCrack.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 11:
+                        i = new Intent(cnt, FlexureCrack1.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 12:
+                        i = new Intent(cnt, FlexureCrack2.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 13:
+                        i = new Intent(cnt, FlexureCrack3.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 14:
+                        i = new Intent(cnt, FlexureCrack4.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 15:
+                        i = new Intent(cnt, HeavingConcreteCrack.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 16:
+                        i = new Intent(cnt, HeavingConcreteCrack1.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 17:
+                        i = new Intent(cnt, HorizontalCrack.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 18:
+                        i = new Intent(cnt, HorizontalCrack1.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 19:
+                        i = new Intent(cnt, Overload.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 20:
+                        i = new Intent(cnt, PlasticShrinkage.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 21:
+                        i = new Intent(cnt, PlasticShrinkage1.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 22:
+                        i = new Intent(cnt, PrematureDrying.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 23:
+                        i = new Intent(cnt, Settling.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 24:
+                        i = new Intent(cnt, Settling1.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 25:
+                        i = new Intent(cnt, ShearCrack.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 26:
+                        i = new Intent(cnt, SteppedCrack.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 27:
+                        i = new Intent(cnt, SteppedCrack1.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 28:
+                        i = new Intent(cnt, SteppedCrack2.class);
+                        cnt.startActivity(i);
+                        break;
+                    case 29:
+                        i = new Intent(cnt, TorsionalCrack.class);
+                        cnt.startActivity(i);
+                        break;
                     default:
                         break;
                 }
